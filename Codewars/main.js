1926bdb6d1e25eaecbe20973b6f1da1aab66042d
Nichole McGrew <nicholemcgrew@gmail.com>
2022-12-04 07:17:52 -0600

    '12'

--- Codewars/main.js
+++ Codewars/main.js
@@ -406,5 +406,147 @@
 
 // CSV representation of array
 
-const toCsvText = array =>
-    array.join('\n')
+// const toCsvText = array =>
+//     array.join('\n')
+
+Training JS #1: create your first JS function and print "Hello World!"
+
+    function helloWorld(){
+        let str = 'Hello World!'
+        console.log(str)
+}
+
+Training JS #2: Basic data types--Number
+
+let v1 = 50,
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250;
+
+function equal1(){
+  let a = v1,   
+      b = v1;   
+  return a + b;
+}
+
+//Please refer to the example above to complete the following functions
+function equal2(){
+  let a = v4 , //set number value to a
+      b = v2 ; //set number value to b
+  return a - b;
+}
+
+function equal3(){
+  let a = v1, //set number value to a
+      b = v5 ; //set number value to b
+  return a * b;
+}
+
+function equal4(){
+  let a = v4 , //set number value to a
+      b = v5 ; //set number value to b
+  return a / b;
+}
+
+function equal5(){
+  let a = v2 , //set number value to a
+      b = v6 ; //set number value to b
+  return a % b;
+}
+
+Basic Training: Add item to an Array
+
+var websites = [];
+websites.push ('codewars')
+console.log(websites)
+
+Training JS #3: Basic data types--String
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return d1 + a2 +d2;
+}
+function Bee(){
+  //select some variable to combine "Bee"
+  return b1 + e2 + e2;
+}
+function banana(){
+  //select some variable to combine "banana"
+  return b2+a2+n2+a2+n2+a2;
+}
+//answer some questions if you finished works above
+function answer1(){
+    //the answer should be "yes" or "no"
+    return "yes";
+  }
+  function answer2(){
+    //the answer should be "yes" or "no"
+    return "yes";
+  }
+  function answer3(){
+    //the answer should be "yes" or "no"
+    return "yes";
+  }
+
+Training JS #4: Basic data types--Array
+
+function getLength(arr){
+    //return length of arr
+    return arr.length
+  }
+
+  function getFirst(arr){
+    //return the first element of arr
+    return arr[0]
+  }
+  
+  function getLast(arr){
+    //return the last element of arr
+    return arr[arr.length - 1]
+  }
+
+  function pushElement(arr){
+    var el=1;
+    //push el to arr
+    arr.push(el)
+    return arr
+  }
+
+  function popElement(arr){
+    //pop an element from arr
+    arr.pop()
+    return arr
+  }
+  
+Training JS #7: if..else and ternary operator
+
+function saleHotdogs(n){
+    if(n < 5) {
+        return 100
+    } else if(n >= 5 && n < 10) {
+        return 95
+    } else if(n >= 10) {
+        return 90
+    }
+  }
+
+  function saleHotdogs(n){
+    return n < 5 ? n*100 : n >= 5 && n < 10 ? n*95 : n*90
+  }
+
+Training JS #10: loop statement --for
+
+function pickIt(arr){
+    var odd=[],even=[];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 !== 0){
+            odd.push(arr[i])
+        } else {
+            even.push(arr[i])
+        }
+    }
+    return [odd,even];
+  }
